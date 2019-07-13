688dea6c37acf8b60bc42f227ab9c897186316d3
water <1351118308@qq.com>
2019-07-13 22:51:20 +0800

    第六天学习

---
+++ Python_LearnNotes/Day06/Note1.py
@@ -0,0 +1,163 @@
+"""
+
+类(Class): 用来描述具有相同的属性和方法的对象的集合。它定义了该集合中每个对象所共有的属性和方法。对象是类的实例。
+类变量：类变量在整个实例化的对象中是公用的。类变量定义在类中且在函数体之外。类变量通常不作为实例变量使用。
+数据成员：类变量或者实例变量用于处理类及其实例对象的相关的数据。
+方法重写：如果从父类继承的方法不能满足子类的需求，可以对其进行改写，这个过程叫方法的覆盖（override），也称为方法的重写。
+局部变量：定义在方法中的变量，只作用于当前实例的类。
+实例变量：在类的声明中，属性是用变量来表示的。这种变量就称为实例变量，是在类声明的内部但是在类的其他成员方法之外声明的。
+继承：即一个派生类（derived class）继承基类（base class）的字段和方法。继承也允许把一个派生类的对象作为一个基类对象对待。例如，有这样一个设计：一个Dog类型的对象派生自Animal类，这是模拟"是一个（is-a）"关系（例图，Dog是一个Animal）。
+实例化：创建一个类的实例，类的具体对象。
+方法：类中定义的函数。
+对象：通过类定义的数据结构实例。对象包括两个数据成员（类变量和实例变量）和方法。
+
+
+构造方法，可以为其添加参数；类方法必须包含self,且为第一个参数
+def __init__(self):
+    self.data = []
+
+继承：分为单继承和多继承
+单继承
+#类定义
+class people:
+    #定义基本属性
+    name = ''
+    age = 0
+    #定义私有属性,私有属性在类外部无法直接进行访问
+    __weight = 0
+    #定义构造方法
+    def __init__(self,n,a,w):
+        self.name = n
+        self.age = a
+        self.__weight = w
+    def speak(self):
+        print("%s 说: 我 %d 岁。" %(self.name,self.age))
+
+#单继承示例
+class student(people):
+    grade = ''
+    def __init__(self,n,a,w,g):
+        #调用父类的构函
+        people.__init__(self,n,a,w)
+        self.grade = g
+    #覆写父类的方法
+    def speak(self):
+        print("%s 说: 我 %d 岁了，我在读 %d 年级"%(self.name,self.age,self.grade))
+
+多继承：
+#类定义
+class people:
+    #定义基本属性
+    name = ''
+    age = 0
+    #定义私有属性,私有属性在类外部无法直接进行访问
+    __weight = 0
+    #定义构造方法
+    def __init__(self,n,a,w):
+        self.name = n
+        self.age = a
+        self.__weight = w
+    def speak(self):
+        print("%s 说: 我 %d 岁。" %(self.name,self.age))
+
+#单继承示例
+class student(people):
+    grade = ''
+    def __init__(self,n,a,w,g):
+        #调用父类的构函
+        people.__init__(self,n,a,w)
+        self.grade = g
+    #覆写父类的方法
+    def speak(self):
+        print("%s 说: 我 %d 岁了，我在读 %d 年级"%(self.name,self.age,self.grade))
+
+#另一个类，多重继承之前的准备
+class speaker():
+    topic = ''
+    name = ''
+    def __init__(self,n,t):
+        self.name = n
+        self.topic = t
+    def speak(self):
+        print("我叫 %s，我是一个演说家，我演讲的主题是 %s"%(self.name,self.topic))
+
+#多重继承
+class sample(speaker,student):
+    a =''
+    def __init__(self,n,a,w,g,t):
+        student.__init__(self,n,a,w,g)
+        speaker.__init__(self,n,t)
+
+test = sample("Tim",25,80,4,"Python")
+test.speak()   #方法名同，默认调用的是在括号中排前地父类的方法
+
+
+方法重写：如果你的父类方法的功能不能满足你的需求，你可以在子类重写你父类的方法
+类的私有属性：__private_attrs：两个下划线开头，声明该属性为私有，不能在类地外部被使用或直接访问。在类内部的方法中使用时self.__private_attrs。
+
+类的专有方法：
+    __init__ : 构造函数，在生成对象时调用
+    __del__ : 析构函数，释放对象时使用
+    __repr__ : 打印，转换
+    __setitem__ : 按照索引赋值
+    __getitem__: 按照索引获取值
+    __len__: 获得长度
+    __cmp__: 比较运算
+    __call__: 函数调用
+    __add__: 加运算
+    __sub__: 减运算
+    __mul__: 乘运算
+    __div__: 除运算
+    __mod__: 求余运算
+    __pow__: 乘方
+
+
+使用__slots__
+但是，如果我们想要限制class的属性怎么办？比如，只允许对Student实例添加name和age属性。
+
+为了达到限制的目的，Python允许在定义class的时候，定义一个特殊的__slots__变量，来限制该class能添加的属性：
+
+>>> class Student(object):
+...     __slots__ = ('name', 'age') # 用tuple定义允许绑定的属性名称
+...
+然后，我们试试：
+
+>>> s = Student() # 创建新的实例
+>>> s.name = 'Michael' # 绑定属性'name'
+>>> s.age = 25 # 绑定属性'age'
+>>> s.score = 99 # 绑定属性'score'
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+AttributeError: 'Student' object has no attribute 'score'
+由于'score'没有被放到__slots__中，所以不能绑定score属性，试图绑定score将得到AttributeError的错误。
+
+使用__slots__要注意，__slots__定义的属性仅对当前类起作用，对继承的子类是不起作用的：
+
+>>> class GraduateStudent(Student):
+...     pass
+...
+>>> g = GraduateStudent()
+>>> g.score = 9999
+除非在子类中也定义__slots__，这样，子类允许定义的属性就是自身的__slots__加上父类的__slots__。
+
+
+__str__方法：
+使用：如：
+class Car:
+    def __init__(self, newWheelNum, newColor):
+        self.wheelNum = newWheelNum
+       	self.color = newColor
+    def __str__(self):
+        msg = "嘿。。。我的颜色是" + self.color + "我有" + int(self.wheelNum) + "个轮胎..."
+        return msg
+    def move(self):
+        print('车在跑，目标:夏威夷')
+BMW = Car(4, "白色")
+print(BMW)
+     总结：
+在python中方法名如果是__xxxx__()的，那么就有特殊的功能，因此叫做“魔法”方法
+当使用print输出对象的时候，只要自己定义了__str__(self)方法，那么就会打印从在这个方法中return的数据
+
+
+
+"""
